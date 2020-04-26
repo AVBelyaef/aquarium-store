9e8f5882528dde188734c14bc6a3afe4cd23d43f
AVBelyaef <avbelyaef@ya.ru>
2020-04-26 23:32:26 +0300

    user model

--- server/server.js
+++ server/server.js
@@ -3,6 +3,14 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
